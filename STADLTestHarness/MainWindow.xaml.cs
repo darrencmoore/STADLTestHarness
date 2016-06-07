a79c9989a90d50d86df52a8ac43f5ff79e8b0336
Darren Moore <darrenm@360shetmetal.com>
2016-06-07 14:09:39 -0700

    added print and hidded print functionality

--- STADLTestHarness/MainWindow.xaml.cs
+++ STADLTestHarness/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using System.Windows.Shapes;
 using STADL;
 using System.Configuration;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+
 
 namespace STADLTestHarness
 {
@@ -30,31 +33,45 @@ namespace STADLTestHarness
         }
 
         private void DBConnectButton_Click(object sender, RoutedEventArgs e)
-        {            
-            STADL.Bot _stadlbotOpenConnection = new STADL.Bot();
-            _stadlbotOpenConnection.DBConnection();
-            if (_stadlbotOpenConnection.DBOpenConnection() == true)
+        {   
+            try
             {
-                MessageBox.Show("Database Connected");
+                STADL.Bot _stadlbotOpenConnection = new STADL.Bot();
+                _stadlbotOpenConnection.DBConnection();
+                if (_stadlbotOpenConnection.DBOpenConnection() == true)
+                {
+                    Console.WriteLine("Database Connected");
+                }
+                else
+                {
+                    Console.WriteLine("Database Not Connected");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Database Not Connected");
+                Console.WriteLine("Error When Connectiing To Database Connection {0}", ex.ToString());
             }
         }       
 
         private void DBCloseButton_Click(object sender, RoutedEventArgs e)
-        {            
-            STADL.Bot _stadlbotCloseConnection = new STADL.Bot();            
-            _stadlbotCloseConnection.DBCloseConnection();
-            if (_stadlbotCloseConnection.DBCloseConnection() == true)
+        {
+            try
             {
-                MessageBox.Show("Database Not Connected");
-            }
-            else
+                STADL.Bot _stadlbotCloseConnection = new STADL.Bot();
+                _stadlbotCloseConnection.DBCloseConnection();
+                if (_stadlbotCloseConnection.DBCloseConnection() == true)
+                {
+                    Console.WriteLine("Database Not Connected");
+                }
+                else
+                {
+                    Console.WriteLine("Database Still Connected");
+                }
+            } 
+            catch (Exception ex)
             {
-                MessageBox.Show("Database Still Connected");
-            }
+                Console.WriteLine("Error When Closing Database Connection {0}", ex.ToString());
+            }           
         }
 
 
@@ -63,35 +80,50 @@ namespace STADLTestHarness
         /// I pass the file name in hard coded from C: this could be taken from user also. 
         /// </summary>
         /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        /// <param name="e"></param>        
+        private void HiddenPrintButton_Click(object sender, RoutedEventArgs e)
         {
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.Verb = "print";
-            info.FileName = @"c:\sample.pdf";
-            info.CreateNoWindow = true;
-            info.WindowStyle = ProcessWindowStyle.Normal;
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.Verb = "print";
+                info.FileName = @"c:\sample.pdf";
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Normal;
 
-            ////Load Files in Selected Folder
-            //string[] allFiles = System.IO.Directory.GetFiles(Directory);
-            //foreach (string file in allFiles)
-            //{
-            //    info.FileName = @file;
-            //    info.CreateNoWindow = true;
-            //    info.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                ////Load Files in Selected Folder
+                //string[] allFiles = System.IO.Directory.GetFiles(Directory);
+                //foreach (string file in allFiles)
+                //{
+                //    info.FileName = @file;
+                //    info.CreateNoWindow = true;
+                //    info.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
 
-            //    p.StartInfo = info;
-            //    p.Start();
-            //}
+                //    p.StartInfo = info;
+                //    p.Start();
+                //}
 
-            Process p = new Process();
-            p.StartInfo = info;
-            p.Start();
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
 
-            p.WaitForInputIdle();
-            System.Threading.Thread.Sleep(3000);
-            if (false == p.CloseMainWindow())
-                p.Kill();
+                p.WaitForInputIdle();
+                System.Threading.Thread.Sleep(3000);
+                if (false == p.CloseMainWindow())
+                    p.Kill();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error When Printing {0}", ex.ToString());
+            }            
+        }
+
+        private void DialogPrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog dlg = new PrintDialog();
+           
+            dlg.ShowDialog();
+
         }
     }
 }
